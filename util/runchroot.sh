f55d1e49fa334033cb5b5c44e7af37c1a21c3c4e
David Oberhollenzer <goliath@infraroot.at>
2019-11-02 21:14:17 +0100

    Don't give anyone in the intermediate container access to /dev/[u]random
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- util/runchroot.sh
+++ util/runchroot.sh
@@ -6,7 +6,7 @@ shift
 BUILDROOT="$1"
 shift
 
-for dev in console full null ptmx random tty urandom zero; do
+for dev in console full null ptmx tty zero; do
 	touch "$SYSROOT/dev/$dev"
 	mount --bind "/dev/$dev" "$SYSROOT/dev/$dev"
 done
