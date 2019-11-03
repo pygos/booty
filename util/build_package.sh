a5c199451e142c4d3e95f52ab6431b071b88d0d1
David Oberhollenzer <goliath@infraroot.at>
2019-11-03 01:55:01 +0100

    Some build speed improvements
    Inside the container, mount/unmount a tmpfs to the build directory
    for the build. Significantly speeds up build time.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- util/build_package.sh
+++ util/build_package.sh
@@ -3,12 +3,23 @@ build_package() {
 		return
 	fi
 
-	fetch_package
 	mkdir -p "$SYSROOT" "$PKGBUILDDIR"
+
+	if [ -z "$PYGOS_BUILD_CONTAINER" ]; then
+		fetch_package
+	else
+		mount -t tmpfs none "$PKGBUILDDIR"
+	fi
+
 	run_pkg_command "build"
 	run_pkg_command "deploy"
 	deploy_dev_cleanup
-	rm -rf "$PKGBUILDDIR"
+
+	if [ -z "$PYGOS_BUILD_CONTAINER" ]; then
+		rm -rf "$PKGBUILDDIR"
+	else
+		umount "$PKGBUILDDIR"
+	fi
 
 	touch "$PKGLOGDIR/${PKGNAME}.done"
 }
