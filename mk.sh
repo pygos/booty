0b18fc611484d7c799be6f7a48a1f5569858a456
David Oberhollenzer <goliath@infraroot.at>
2019-11-05 14:20:19 +0100

    Cleanup: split curl/cabundle into seperate packages
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- mk.sh
+++ mk.sh
@@ -49,8 +49,8 @@ export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
 export PKG_CONFIG_LIBDIR="$SYSROOT/lib/pkgconfig"
 export PKG_CONFIG_PATH="$SYSROOT/lib/pkgconfig"
 
-for pkg in basefiles ncurses readline bash xz make zlib file busybox flex \
-	   bison gmp mpfr mpc openssl curl squashfs-tools-ng patch perl5 \
+for pkg in basefiles cabundle ncurses readline bash xz make zlib file busybox \
+	   flex bison gmp mpfr mpc openssl curl squashfs-tools-ng patch perl5 \
 	   rhash expat libarchive jsoncpp libuv binutils gcc cmake m4 \
 	   autoconf autoconf-archive automake pkg-config libtool; do
 	include_pkg "$pkg"
