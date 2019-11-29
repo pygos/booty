66df82042ed675268b096a122d8ec0b70546d13c
David Oberhollenzer <goliath@infraroot.at>
2019-11-29 14:38:51 +0100

    Resurrect bzip2
    Its a build dependency of cmake and we REALLY DON'T want cmake to build
    its own, internal version.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- mk.sh
+++ mk.sh
@@ -51,9 +51,9 @@ export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
 export PKG_CONFIG_LIBDIR="$SYSROOT/lib/pkgconfig"
 export PKG_CONFIG_PATH="$SYSROOT/lib/pkgconfig"
 
-for pkg in basefiles cabundle ncurses xz make zlib file busybox flex bison gmp\
-	   mpfr mpc openssl curl squashfs-tools-ng patch perl5 rhash expat \
-	   libarchive jsoncpp libuv binutils gcc cmake m4 autoconf \
+for pkg in basefiles cabundle ncurses xz make zlib file bzip2 busybox \
+	   flex bison gmp mpfr mpc openssl curl squashfs-tools-ng patch perl5\
+	   rhash expat libarchive jsoncpp libuv binutils gcc cmake m4 autoconf\
 	   autoconf-archive automake pkg-config libtool; do
 	include_pkg "$pkg"
 	build_package
