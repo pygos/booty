917eb04389db73c3e59bf9703487f62e5a6f398e
David Oberhollenzer <goliath@infraroot.at>
2019-11-29 15:23:20 +0100

    Resurrect gzip and GNU tar
    GNU tar is faster and has some features that busybox tar simply lacks.
    
    Also, we already build xz and bzip2 because we need them, so why not just
    add gzip to the list and completely disable compressor/packer support in
    busybox.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- mk.sh
+++ mk.sh
@@ -51,10 +51,10 @@ export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
 export PKG_CONFIG_LIBDIR="$SYSROOT/lib/pkgconfig"
 export PKG_CONFIG_PATH="$SYSROOT/lib/pkgconfig"
 
-for pkg in basefiles cabundle ncurses xz make zlib file bzip2 zstd busybox \
-	   flex bison gmp mpfr mpc openssl curl squashfs-tools-ng patch perl5\
-	   rhash expat libarchive jsoncpp libuv binutils gcc cmake m4 autoconf\
-	   autoconf-archive automake pkg-config libtool; do
+for pkg in basefiles cabundle ncurses xz make zlib file gzip bzip2 zstd \
+	   tar busybox flex bison gmp mpfr mpc openssl curl squashfs-tools-ng \
+	   patch perl5 rhash expat libarchive jsoncpp libuv binutils gcc cmake\
+	   m4 autoconf autoconf-archive automake pkg-config libtool; do
 	include_pkg "$pkg"
 	build_package
 done
