ca115f4b5fd3b1f4f89b3843b520b4f40bfb4a7c
David Oberhollenzer <goliath@infraroot.at>
2019-11-29 14:53:08 +0100

    Add zstd package
    Its a build dependency for cmake and can also be used by squashfs-tools-ng.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- mk.sh
+++ mk.sh
@@ -51,7 +51,7 @@ export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
 export PKG_CONFIG_LIBDIR="$SYSROOT/lib/pkgconfig"
 export PKG_CONFIG_PATH="$SYSROOT/lib/pkgconfig"
 
-for pkg in basefiles cabundle ncurses xz make zlib file bzip2 busybox \
+for pkg in basefiles cabundle ncurses xz make zlib file bzip2 zstd busybox \
 	   flex bison gmp mpfr mpc openssl curl squashfs-tools-ng patch perl5\
 	   rhash expat libarchive jsoncpp libuv binutils gcc cmake m4 autoconf\
 	   autoconf-archive automake pkg-config libtool; do
